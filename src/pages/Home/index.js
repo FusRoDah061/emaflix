8d03ef56714f65254b5080cb8312d9cbf303eafb
Allex Rodrigues <allexxrodriguess@gmail.com>
2020-07-30 19:55:22 -0300

    Deploy heroku

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import Menu from '../../components/Menu';
 import dadosIniciais from '../../data/dados_iniciais.json';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
-import Footer from '../../components/Footer';
 import PageDefault from '../../components/PageDefault';
 
 function Home() {
@@ -13,7 +11,7 @@ function Home() {
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"O que é Front-end? Trabalhando na área"} 
+        videoDescription="O que é Front-end? Trabalhando na área"
       />
 
       <Carousel
